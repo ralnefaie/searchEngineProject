5ae840c1207491b4d8128751c1fa324d45ccb763
al-radi@hotmail.com <>
2015-03-02 02:33:54 +0500

    addedFileChooser

---
+++ SearchEngine/src/searchengine/addingFileToIndex.java
@@ -0,0 +1,155 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searchengine;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Ali
+ */
+public class addingFileToIndex extends JPanel
+                             implements ActionListener {
+    static private final String newline = "\n";
+    JButton openButton, saveButton;
+    JTextArea log;
+    JFileChooser fc;
+ 
+    public addingFileToIndex() {
+        super(new BorderLayout());
+ 
+        //Create the log first, because the action listeners
+        //need to refer to it.
+        log = new JTextArea(5,20);
+        log.setMargin(new Insets(5,5,5,5));
+        log.setEditable(false);
+        JScrollPane logScrollPane = new JScrollPane(log);
+ 
+        //Create a file chooser
+        fc = new JFileChooser();
+ 
+        openButton = new JButton("Open a File...",
+                                 createImageIcon("images/Open16.gif"));
+        openButton.addActionListener(this);
+ 
+        //Create the save button.  We use the image from the JLF
+        //Graphics Repository (but we extracted it from the jar).
+        saveButton = new JButton("Save a File...",
+                                 createImageIcon("images/Save16.gif"));
+        saveButton.addActionListener(this);
+        
+        
+        
+        
+        
+        
+ 
+        //For layout purposes, put the buttons in a separate panel
+        JPanel buttonPanel = new JPanel(); //use FlowLayout
+        buttonPanel.add(openButton);
+        buttonPanel.add(saveButton);
+ 
+        //Add the buttons and the log to this panel.
+        add(buttonPanel, BorderLayout.PAGE_START);
+        add(logScrollPane, BorderLayout.CENTER);
+    }
+ 
+    public void actionPerformed(ActionEvent e) {
+ 
+        //Handle open button action.
+        if (e.getSource() == openButton) {
+            int returnVal = fc.showOpenDialog(addingFileToIndex.this);
+ 
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //This is where a real application would open the file.
+                log.append("Opening: " + file.getName() + "." + newline);
+            } else {
+                log.append("Open command cancelled by user." + newline);
+            }
+            log.setCaretPosition(log.getDocument().getLength());
+ 
+        //Handle save button action.
+        } else if (e.getSource() == saveButton) {
+            int returnVal = fc.showSaveDialog(addingFileToIndex.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //This is where a real application would save the file.
+                log.append("Saving: " + file.getName() + "." + newline);
+            } else {
+                log.append("Save command cancelled by user." + newline);
+            }
+            log.setCaretPosition(log.getDocument().getLength());
+        }
+    }
+ 
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = addingFileToIndex.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+    
+    
+    
+    /*
+     * 
+     * Stub to delete a specific file
+     */
+    public boolean DeleteAfile( File f){
+    
+    
+        return true;
+    }
+ 
+    public static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("FileChooserDemo");
+        frame.setPreferredSize(new Dimension(500, 400));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Add content to the window.
+        frame.add(new addingFileToIndex());
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public static boolean addingFileToGitNewFile(File fil){
+
+//        stub for adding a file
+        return false;
+    }
+   
+
+    
+        public static boolean deletingFileFromGitNewFile(File fil){
+        
+//        stub for adding a file
+        return false;
+    }
+   
+        
+        
+ 
+
+}
