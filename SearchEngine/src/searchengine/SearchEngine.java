3f775b4e8b845a5dc6e97f05f14ea016c1d24348
faisalalmuammar@gmail.com <>
2015-03-02 02:25:48 +0500

    mySearchEngineGUI

---
+++ SearchEngine/src/searchengine/SearchEngine.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searchengine;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Ali
+ */
+public class SearchEngine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+            /**
+     * @param args the command line arguments
+     */
+  
+        // TODO code application logic here
+        try { 
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+} catch (Exception e) {
+    e.printStackTrace();
+}  
+        GUIsearch searchGUI = new GUIsearch();
+        searchGUI.setVisible(true);
+    
+    }
+}
